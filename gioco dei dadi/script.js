53ff5e2f0c73f7db57c01160e019f5e93613b9ee
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-04 14:31:41 +0100

    es gioco dei dadi impostato

---
+++ gioco dei dadi/script.js
@@ -0,0 +1,16 @@
+"use String"
+
+// Consegna
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Genera un numero random per il giocatore 
+
+
+// Genera un numero random per il compiuter
+
+// Genera vincitore 
+
+
