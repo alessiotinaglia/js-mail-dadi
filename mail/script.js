bd8e46045304c173b1220405d6c6ba3f8e85c640
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-04 14:03:09 +0100

    inpostazione es mail

---
+++ mail/script.js
@@ -0,0 +1,35 @@
+"use String"
+
+// Consegna
+
+// Mail
+
+// Chiedi all’utente la sua email,
+
+// controlla che sia nella lista di chi può accedere,
+
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// Non è necessario provvedere alla validazione delle email
+
+
+// Esercizio
+
+// Creare delle mail tipo, che poi l'utente dovra inserire
+
+// Chiediamo all'utente di inserire la sua mail 
+
+// verifichiamo se la mail dell'utente e nella lista di quelle che può accedere 
+
+// stampiamo due messaggi, se e valida o se non e valida 
+
+
+
+
+
+
+
+
+
+
+
