30f6fd73ca3371f896f4dfc733cfbabac2ad7764
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-04 14:56:20 +0100

    primo punto secondo es

--- gioco dei dadi/script.js
+++ gioco dei dadi/script.js
@@ -3,14 +3,19 @@
 // Consegna
 
 // Gioco dei dadi
+
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+
+// Esercizio :
+
 // Genera un numero random per il giocatore 
+const punteggioGiocatore = Math.floor(Math.random() * 6) + 1;
+console.log("Il tuo numero : " + punteggioGiocatore);
 
+// Genera un numero random per il computer
 
-// Genera un numero random per il compiuter
 
 // Genera vincitore 
-
-
